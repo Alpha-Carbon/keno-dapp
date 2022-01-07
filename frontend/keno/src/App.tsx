e2ba49f7ef08438d83185c215c31b739ba9ca37c
tonyz840101 <tonyz840101@gmail.com>
2022-01-07 15:01:28 +0800

    fix wrong block number in verify url

--- frontend/keno/src/App.tsx
+++ frontend/keno/src/App.tsx
@@ -47,7 +47,7 @@ const KenoContainer: React.FC<KenoContainerProps> = ({ container, rule, contract
         className="game-controller"
       />
       <div className="verify-btn">
-        <a href={`https://oracle-app.alphacarbon.network/verify/keno?address=${contract?.address}&block=${currentBlock}&round=${currentRoundResult?.round}&chainId=31337`}
+        <a href={`https://oracle-app.alphacarbon.network/verify/keno?address=${contract?.address}&block=${currentRoundResult?.round * rule?.drawRate.toNumber()}&round=${currentRoundResult?.round}&chainId=31337`}
           target="_blank"
           rel="noopener noreferrer">Verify</a>
       </div>
